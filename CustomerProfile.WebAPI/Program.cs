f7b60bdbc2a6d31b457bbc9b79ad1758f7f62c6a
Nirav04-hub <nsaxena9968@conestogac.on.ca>
2026-02-13 10:44:45 -0500

    CarRenatal Project From last lab

---
+++ CustomerProfile.WebAPI/Program.cs
@@ -0,0 +1,30 @@
+using CustomerProfile.WebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<CustmerProfileContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CustomerProfile")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
